33f7fb61db0c20361b531e336914319df41dafdf
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-08-06 20:42:32 -0700

    added full CRUD functionality for contacts and users

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,24 +1,44 @@
 class ContactsController < ApplicationController
+  
   def index
     @contacts = Contact.all
   end
 
   def show
+    @contact = Contact.find(params[:id])
   end
 
   def new
+    @contact = Contact.new
+    @is_signup = true
   end
 
   def create
-    current_user.contacts.create(params.require(contact).permit(:fname, :lname, :spouse, :email, :home_phone, :mobile_phone, :address, :twitter, :giving_level))
+    @contact = current_user.contacts.create(params.require(:contact).permit(:fname, :lname, :spouse, :email, :home_phone, :mobile_phone, :address, :twitter, :giving_level))
+
+    if @contact.save
+      redirect_to contacts_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @contact = Contact.find(params[:id])
   end
 
   def update
+    @contact = Contact.find(params[:id])
+    if @contact.update_attributes(params.require(:contact).permit(:fname, :lname, :spouse, :email, :home_phone, :mobile_phone, :address, :twitter, :giving_level))
+      redirect_to contacts_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to contacts_path
   end
 end
