0715b0d24157ab39bffc1dacd860b6777729c3d6
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-08-14 21:34:40 -0700

    fixed issue where touchpoints were being shown as overdue too early

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,12 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.where(:id => session[:user_id]).first
   end
+
+  # around_filter :user_time_zone, :if => :current_user
+
+  # def user_time_zone(&block)
+  #   Time.use_zone(current_user.time_zone, &block)
+  # end
+
 end
 
