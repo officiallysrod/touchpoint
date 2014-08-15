59733dccdb9b4692e6583c609f7ac099989569db
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-08-15 12:36:00 -0700

    added tabbed navigation on the dashboard for new touchpoint and new contact

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @contacts = @user.contacts.all
+    @touch = Touch.new
 
     if @user == current_user
       @user
